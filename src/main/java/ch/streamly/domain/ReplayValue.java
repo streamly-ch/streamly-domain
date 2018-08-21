4088e8a9c463bff846479e114fd849de3281c9cc
mgabriel <github@mgabriel.com>
2018-08-21 14:09:39 +0200

    create default constructor

--- src/main/java/ch/streamly/domain/ReplayValue.java
+++ src/main/java/ch/streamly/domain/ReplayValue.java
@@ -2,7 +2,6 @@ package ch.streamly.domain;
 
 /**
  * A value wrapper that indicates if the current value is the first value replayed in the replay loop.
- * @see ReplayInLoop
  * @param <T> data type
  */
 public interface ReplayValue<T> extends WrappedValue<T> {
@@ -11,4 +10,22 @@ public interface ReplayValue<T> extends WrappedValue<T> {
      * @return true if this object is the loop restart signal (meaning that the replay loop has restarted from the beginning)
      */
     boolean isLoopRestart();
+
+    /**
+     *
+     * @param value the value to wrap.
+     * @return a ReplayValue that is not a loop restart.
+     */
+    default ReplayValue newValue(T value){
+        return new ReplayValueImpl<>(value);
+    }
+
+    /**
+     *
+     * @param value the value to wrap.
+     * @return a loop restart value.
+     */
+    default ReplayValue newLoopRestartValue(T value){
+        return new ReplayValueImpl<>(true, value);
+    }
 }
